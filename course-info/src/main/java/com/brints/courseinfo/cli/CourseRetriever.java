bcb30b45ead83203ad0499e8b86b3cfb35d38aad
aniebietafia <aniebietafia87@gmail.com>
2024-08-26 14:45:46 +0100

    adding logging dependency

---
+++ course-info/src/main/java/com/brints/courseinfo/cli/CourseRetriever.java
@@ -0,0 +1,26 @@
+package com.brints.courseinfo.cli;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CourseRetriever {
+    private static final Logger LOG = LoggerFactory.getLogger(CourseRetriever.class);
+
+    public static void main(String[] args) {
+        LOG.info("App starting on http://localhost:8080/");
+        if (args.length == 0) {
+            LOG.warn("Please provide an author name as first argument.");
+            return;
+        }
+        
+        try {
+            retrieveCourses(args[0]);
+        } catch (Exception e) {
+            LOG.error(e.getMessage());
+        }
+    }
+
+    private static void retrieveCourses(String authorId) {
+        System.out.println("Retrieving courses for author " + authorId);
+    }
+}
